5af8b7bdaeb0fbd47db0460de677e61b1eeef904
qinghuanI <1062217965@qq.com>
2022-05-07 07:10:10 +0800

    add button

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,17 +5,14 @@ import Page2 from "../pages/page2";
 const routes = [
     {
         path: '/',
-        exact: true,
         element: <Navigate to="/page1"/>
     },
     {
         path: '/page1',
-        exact: true,
         element: <Page1/>
     },
     {
         path: '/page2',
-        exact: true,
         element: <Page2/>
     },
 ]
